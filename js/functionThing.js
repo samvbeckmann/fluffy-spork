cab23e8562d54c4b93edbf5677d07778d8cbfc3b
DasAlvarp <alvaroguis@gmail.com>
2015-11-14 21:45:36 -0600

    added :diamond_shape_with_a_dot_inside:

--- js/functionThing.js
+++ js/functionThing.js
@@ -1,16 +1,19 @@
 function removeSpaces()
 {
 	/*
+		var img = document.createElement("img");
+		img.src = "http://www.emoji-cheat-sheet.com/graphics/emojis/" + document.getElementById('comment').text + ".png";
+	*/
+
+	var wee = "http://www.emoji-cheat-sheet.com/graphics/emojis/" + document.getElementById('comment').value + ".png";
+
+
 	var img = document.createElement("img");
-	img.src = "http://www.emoji-cheat-sheet.com/graphics/emojis/" + document.getElementById('comment').text + ".png";
+	img.src = wee
 
-*/
-		alert("http://www.emoji-cheat-sheet.com/graphics/emojis/" + document.getElementById('comment').value + ".png")
+	var src = document.getElementById("wooloo")
 
-	var img = document.createElement("img");
-
-	var wee = "http://www.emoji-cheat-sheet.com/graphics/emojis/smile.png";
-
-	$('#output').prepend('<img id="img" src=wee />')
+	src.appendChild(img)
+	alert(wee)
 
 }
