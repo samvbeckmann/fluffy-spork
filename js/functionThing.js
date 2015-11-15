fac8aa0a635523a9431f59b4f31c07c039aae8df
DasAlvarp <alvaroguis@gmail.com>
2015-11-14 20:02:33 -0600

    uh, yeah

---
+++ js/functionThing.js
@@ -0,0 +1,7 @@
+function removeSpaces()
+{
+	var img = document.createElement("img");
+img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+
+	$('#output').prepend('<img id="theImg" src="mrTemp" />')
+}
