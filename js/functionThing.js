be4942b3adae5b90d6c64086d0f7b7a3be5f9c4c
DasAlvarp <alvaroguis@gmail.com>
2015-11-14 20:51:40 -0600

    Made easy thing finally work

--- js/functionThing.js
+++ js/functionThing.js
@@ -1,7 +1,16 @@
 function removeSpaces()
 {
+	/*
 	var img = document.createElement("img");
-img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+	img.src = "http://www.emoji-cheat-sheet.com/graphics/emojis/" + document.getElementById('comment').text + ".png";
 
-	$('#output').prepend('<img id="theImg" src="mrTemp" />')
+*/
+		alert("http://www.emoji-cheat-sheet.com/graphics/emojis/" + document.getElementById('comment').value + ".png")
+
+	var img = document.createElement("img");
+
+	var wee = "http://www.emoji-cheat-sheet.com/graphics/emojis/smile.png";
+
+	$('#output').prepend('<img id="img" src=wee />')
+
 }
